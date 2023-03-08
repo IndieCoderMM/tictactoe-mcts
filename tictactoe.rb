487acccdbe43e262b9482424d6f5c765e94268de
heinthant <hthant00chk@gmail.com>
2023-03-08 11:20:58 +0630

    Add board and game loop

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,49 +1,71 @@
 class TicTacToe
-    def initialize
-        @rows = 3
-        @cols = 3
-        @action_size = @rows * @cols
+  def initialize
+    @rows = 3
+    @cols = 3
+    @action_size = @rows * @cols
+  end
+
+  def get_initial_state
+    Array.new(@rows) { Array.new(@cols) {0}}
+  end
+
+  def get_next_state(state, action, player)
+    row = action / @cols 
+    col = action % @cols 
+    state[row][col] = player 
+    return state 
+  end 
+
+  def get_legal_moves(state)
+    state.flatten.map {|s| s === 0}
+  end 
+
+  def check_win(state, action) 
+    row = action / @cols 
+    col = action % @cols
+    player = state[row][col]
+    is_row_connected = state[row].sum === player * @rows 
+    is_col_connected = state.transpose[col].sum === player * @cols
+    # TODO Check diagonals 
+    return is_row_connected || is_col_connected
+  end 
+
+  def check_gameover(state, action)
+    if check_win(state, action)
+      return true
     end
-
-    def get_initial_state
-        return Array.new(@rows).fill(Array.new(@cols).fill(0))
+    unless get_legal_moves(state).any?
+      return true
     end
+    return false
+  end
 
-    def get_next_state(state, action, player)
-        row = action / @cols 
-        col = action % @cols 
-        state[0][col] = player 
-        puts "#{state} <<< DEGUG"
-        return state 
-    end 
+  def get_opponent(player) = -player
 
-    def get_legal_moves(state)
-        return state.flatten.map {|s| s === 0}
-    end 
-
-    def check_win(state, action) 
-        row = action / @cols 
-        col = action % @cols
-        player = state[row][col]
-        is_row_connected = state[row].sum === player * @rows 
-        is_col_connected = state.transpose[col].sum === player * @cols
-        # TODO Check diagonals 
-        return is_row_connected || is_col_connected
-    end 
-
-    def check_gameover(state, action)
-        # if check_win(state, action)
-        #     return true
-        # end
-        unless get_legal_moves(state).any?
-            return true
-        end
-        return false
+  def print_board(state, title, p1, p2)
+    board_width = 9 * @cols
+    puts 
+    state.each_with_index do |row, r|
+      if r === 0
+        puts title.upcase.center(board_width)
+        puts "-" * (board_width)
+      end
+      row.each_with_index do |val, c|
+          print " | "
+          if val === 1
+            print p1.center(4)
+          elsif val === -1
+            print p2.center(4)
+          else 
+            print "#{c + @cols * r + 1}".center(5)
+          end
+          print " |" if c === @cols - 1
+      end
+      puts 
+      print "-" * (board_width)
+      puts 
     end
-
-    def get_opponent(player)
-        return -player
-    end 
+  end
 end
 
 tictactoe = TicTacToe.new
@@ -51,25 +73,25 @@ player = 1
 state = tictactoe.get_initial_state
 
 while true
-    state.each {|r| puts r.to_s}
-    puts state.to_s
-    legal_moves = tictactoe.get_legal_moves(state)
-    puts "Legal moves =>", (0..8).map {|i| legal_moves[i] ? i+1:nil}.to_s
-    action = gets.chomp.to_i - 1
+  tictactoe.print_board(state, "Tictactoe Master", "😃", "😈")
+  legal_moves = tictactoe.get_legal_moves(state)
+  print "Enter your move => "
+  action = gets.chomp.to_i - 1
 
-    if action === -1
-        break
-    end
+  if action === -1
+    break
+  end
 
-    unless legal_moves[action] 
-        puts "Invalid move!"
-        next 
-    end
+  unless legal_moves[action] 
+    puts "🔴Invalid move!"
+    next 
+  end
 
-    state = tictactoe.get_next_state(state, action, player)
-    if tictactoe.check_gameover(state, action)
-        puts "Game Over"
-        break
-    end
-    player = tictactoe.get_opponent(player)
+  state = tictactoe.get_next_state(state, action, player)
+  if tictactoe.check_gameover(state, action)
+    # TODO Display game over state
+    puts "Game Over"
+    break
+  end
+  player = tictactoe.get_opponent(player)
 end
